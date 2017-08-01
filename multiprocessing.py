df6ddb9ebc5215ce42d34a590276f4eefa7a86f7
Sathyan Murugan <sathyan.murugan@outlook.com>
2017-08-01 20:34:22 +0200

    Create multiprocessing.py

---
+++ multiprocessing.py
@@ -0,0 +1,15 @@
+import multiprocessing
+import datetime,time
+
+def do_something(x):
+    start_time = datetime.datetime.now().strftime('%H:%M:%S')
+    time.sleep(2)
+    return (x,start_time)
+
+list_of_numbers = list(range(0,4))
+
+number_of_cores = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(processes=number_of_cores)
+results = pool.map(do_something, list_of_numbers)
+
+print (results)
